ceddef8adfb575fa4894e1b6dcd5980bfd7236ee
luoye20230624 <137587181+luoye20230624@users.noreply.github.com>
2024-09-19 09:01:09 +0800

    Update ZB.py

--- ZB.py
+++ ZB.py
@@ -252,7 +252,7 @@ def main():
                     in channel_name or '张家界' in channel_name or '怀化' in channel_name or '浏阳' in channel_name or '湘西' \
                     in channel_name or '衡阳' in channel_name or '邵阳' in channel_name or '郴州' in channel_name or '岳阳' in channel_name or '溆浦' \
                     in channel_name or '武冈' in channel_name or '新化' in channel_name or '津市' in channel_name or '桂东' in channel_name \
-                    or '道县' in channel_name or '永州' in channel_name或 '株洲' in channel_name或 '湘潭' in channel_name或 '益阳' in channel_name:
+                    or '道县' in channel_name or '永州' in channel_name or '株洲' in channel_name or '湘潭' in channel_name or '益阳' in channel_name:
                 if channel_counters.get(channel_name, 0) < 10:
                     file.write(f"{channel_name},{channel_url}\n")
                     channel_counters[channel_name] = channel_counters.get(channel_name, 0) + 1
